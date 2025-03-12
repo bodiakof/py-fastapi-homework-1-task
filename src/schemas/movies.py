721b30382d00de0836379cf8276687088f3a2ae5
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-03-12 18:01:23 +0200

    Created schemas

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,35 @@
-# Write your code here
+from datetime import date
+
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class BaseSchema(BaseModel):
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class MovieDetailResponseSchema(BaseSchema):
+    id: int
+    name: str
+    date: date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[str] = None
+    next_page: Optional[str] = None
+    total_pages: int
+    total_items: int
